a549ee3320fda4bc12be836c6ee92d79685c209a
yadavamit2611 <thisisamit.kumar21@gmail.com>
2023-08-04 16:42:41 +0200

    edge cases and test cases handled

--- interview-frontend/src/app/app.component.spec.ts
+++ interview-frontend/src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
+    imports: [RouterTestingModule,
+      HttpClientModule,
+      FormsModule],
+    declarations: [AppComponent],
   }));
 
   it('should create the app', () => {
--- interview-frontend/src/app/app.component.ts
+++ interview-frontend/src/app/app.component.ts
@@ -7,18 +7,29 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'City Finder';
+  title = 'interview-frontend';
   searchQuery: string = '';
   cities: any[] = [];
 
   constructor(private http: HttpClient){
   }
  
-  searchCities() {
-    this.http
-      .get<any[]>(`api/cities?query=${this.searchQuery}`)
-      .subscribe((response:string[]) => {
-        this.cities = response;
-      });
+  searchCities(event: any) {
+    const input = event.target as HTMLInputElement;
+    const regex = /^[a-zA-Z]+$/;
+
+    if (!regex.test(input.value)) {
+      input.value = input.value.replace(/[^a-zA-Z]/g, '');
+      this.searchQuery = input.value;
+    }
+    if(this.searchQuery!==""){
+        this.http
+        .get<any[]>(`api/cities?query=${this.searchQuery}`)
+        .subscribe((response:any[]) => {
+          this.cities = response;
+        });
+    }else{
+      this.cities = [];
+    }
   }
 }
